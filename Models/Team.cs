606274f5401ac209f8b7dea0ace259f7af3401c6
sandratraniaina <sandratrarafa@gmail.com>
2025-01-15 15:21:43 +0300

    feat: Update color to hsv and add min and max

--- Models/Team.cs
+++ Models/Team.cs
@@ -10,13 +10,15 @@ namespace offside_checker.Models
     public class Team
     {
         private List<Player> _players;
-        private Bgr color;
+        private Hsv minColor;
+        private Hsv maxColor;
         private Player goalKeeper;
         private Player playerWithBall;
 
         public List<Player> Players { get => _players; set => _players = value; }
-        public Bgr Color { get => color; set => color = value; }
         public Player GoalKeeper { get => goalKeeper; set => goalKeeper = value; }
         public Player PlayerWithBall { get => playerWithBall; set => playerWithBall = value; }
+        public Hsv MinColor { get => minColor; set => minColor = value; }
+        public Hsv MaxColor { get => maxColor; set => maxColor = value; }
     }
 }
