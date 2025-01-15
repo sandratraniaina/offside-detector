7e31649c8467ef932f0c9a9c4c7b028f48b5c7aa
sandratraniaina <sandratrarafa@gmail.com>
2025-01-15 20:15:00 +0300

    feat: Add isOffside field to Player

--- Models/Player.cs
+++ Models/Player.cs
@@ -11,8 +11,10 @@ namespace offside_checker.Models
     {
         private Point point;
         private Double radius;
+        private bool isOffside;
 
         public double Radius { get => radius; set => radius = value; }
         public Point Point { get => point; set => point = value; }
+        public bool IsOffside { get => isOffside; set => isOffside = value; }
     }
 }
---
+++ Services/OffsideDetector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offside_checker.Services
+{
+    internal class OffsideDetector
+    {
+    }
+}
