205774c29c2b2f45c28c5fdc5b776e271fc3e36e
sandratraniaina <sandratrarafa@gmail.com>
2025-01-15 22:01:25 +0300

    feat: Update color from bgr to hsv

--- Models/Team.cs
+++ Models/Team.cs
@@ -10,15 +10,15 @@ namespace offside_checker.Models
     public class Team
     {
         private List<Player> _players;
-        private Bgr minColor;
-        private Bgr maxColor;
+        private Hsv minColor;
+        private Hsv maxColor;
         private Player goalKeeper;
         private Player playerWithBall;
 
         public List<Player> Players { get => _players; set => _players = value; }
         public Player GoalKeeper { get => goalKeeper; set => goalKeeper = value; }
         public Player PlayerWithBall { get => playerWithBall; set => playerWithBall = value; }
-        public Bgr MinColor { get => minColor; set => minColor = value; }
-        public Bgr MaxColor { get => maxColor; set => maxColor = value; }
+        public Hsv MinColor { get => minColor; set => minColor = value; }
+        public Hsv MaxColor { get => maxColor; set => maxColor = value; }
     }
 }
