d97da5a1a3200e728990a82b31b4391b34d95757
sandratraniaina <sandratrarafa@gmail.com>
2025-01-15 14:56:56 +0300

    feat: Create Team model

---
+++ Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offside_checker.Models
+{
+    public class Team
+    {
+        private List<Player> _players;
+        private Bgr color;
+        private Player goalKeeper;
+        private Player playerWithBall;
+
+        public List<Player> Players { get => _players; set => _players = value; }
+        public Bgr Color { get => color; set => color = value; }
+        public Player GoalKeeper { get => goalKeeper; set => goalKeeper = value; }
+        public Player PlayerWithBall { get => playerWithBall; set => playerWithBall = value; }
+    }
+}
