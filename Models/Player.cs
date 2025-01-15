e6a21195eaaf4d68ad12f9c130c1cda5de9e64f0
sandratraniaina <sandratrarafa@gmail.com>
2025-01-15 11:20:03 +0300

    feat: Create Players model

---
+++ Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace offside_checker.Models
+{
+    public class Player
+    {
+        public Point Position { get; set; }
+        public string Team { get; set; }  // "red" or "blue"
+        public string Status { get; set; } // "HJ" for offside, "M" for between ball and last defense
+        public bool IsGoalkeeper { get; set; }
+    }
+}
